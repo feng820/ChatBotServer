48c91bf5d6018c0d6c5a19f88af4e7bbd13b6563
feng820 <linfengwork820@gmail.com>
2023-04-11 00:30:38 -0700

    Init proj

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  // Stop ESLint from looking for a configuration file in parent folders
+  root: true,
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    // sets up both the prettier plugin and eslint-config-prettier in one go
+    // more info on https://github.com/prettier/eslint-plugin-prettier
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: "@babel/eslint-parser",
+  },
+
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
