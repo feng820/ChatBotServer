fc8d50a64e812304dc01a6719f25cc89c0fa55fd
feng820 <linfengwork820@gmail.com>
2023-04-11 01:01:05 -0700

    More liniting and init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
-"use strict";
+"use strict"
 
 module.exports = {
   // Stop ESLint from looking for a configuration file in parent folders
   root: true,
+
   env: {
     es2020: true,
     node: true,
   },
+
   extends: [
     "eslint:recommended",
     // sets up both the prettier plugin and eslint-config-prettier in one go
@@ -32,5 +34,6 @@ module.exports = {
         "newlines-between": "always",
       },
     ],
+    "prettier/prettier": ["error", { semi: false }],
   },
-};
+}
